1dee04800611bfff8e8ed51bf4bc96ced4fb7a52
JustSia <114462972+JustSia@users.noreply.github.com>
2023-09-27 21:23:23 +1000

    Fixed MongoDB

--- BE/server.js
+++ BE/server.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -6,7 +8,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb+srv://Jape:<8SqqGJIUEx2S7YSI>@cluster0.3wznhov.mongodb.net/?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
+// Use environment variables to keep sensitive information secure
+const DB_USER = process.env.DB_USER;
+const DB_PASS = process.env.DB_PASS;
+
+// Correct the connection string by specifying the database name
+mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASS}@cluster0.3wznhov.mongodb.net/myapp?retryWrites=true&w=majority`, {useNewUrlParser: true, useUnifiedTopology: true});
 
 const SectionSchema = new mongoose.Schema({
   name: String,
@@ -16,17 +23,25 @@ const SectionSchema = new mongoose.Schema({
 const Section = mongoose.model('Section', SectionSchema);
 
 app.get('/sections', async (req, res) => {
-  const sections = await Section.find();
-  res.json(sections);
+  try {
+    const sections = await Section.find();
+    res.json(sections);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
 app.post('/sections', async (req, res) => {
-  const section = new Section(req.body);
-  await section.save();
-  res.json(section);
+  try {
+    const section = new Section(req.body);
+    await section.save();
+    res.json(section);
+  } catch (error) {
+    console.error(error);
+    res.status(400).send('Bad Request');
+  }
 });
-
-// More routes for other CRUD operations...
 
 const port = 3000;
 app.listen(port, () => console.log(`Server is running on http://localhost:${port}`));
